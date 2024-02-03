3eb1b4824bb5b40109bbed016e181907b99ebeeb
kulturman <arnaudbakyono@gmail.com>
2024-02-03 16:17:15 +0000

    Init project

---
+++ src/main/java/com/kulturman/webserver/BasicWebServerApplication.java
@@ -0,0 +1,13 @@
+package com.kulturman.webserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicWebServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasicWebServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kulturman/webserver/BasicWebServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kulturman.webserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicWebServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
